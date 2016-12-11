05a31d6b5ab1b6bdaa3f770b1e3545dd78bd11ec
Ryan <hey@mix-tape.io>
2016-12-11 14:53:40 +0800

    Add some home content

--- wp-content/themes/chopshopmedia/templates/content/home.php
+++ wp-content/themes/chopshopmedia/templates/content/home.php
@@ -18,9 +18,32 @@
   </section>
 
 
-  <section class="wrapper">
+  <section class="wrapper pageSection">
 
     <div class="container">
+
+      <div class="sectionSplit--small">
+        <div class="sectionTitle">
+          <span class="sectionTitle-number">01</span>
+          <span class="sectionTitle-title">Who <br>We Are</span>
+        </div>
+      </div>
+
+      <div class="sectionSplit--large">
+
+        <div class="sectionSplit-content">
+
+          <p><strong>Chop Shop Media is a creative agency specialising in film & content and creative direction.</strong></p>
+
+          <p>Founded by The Rules (Adam and Gemma) in 2008, CSM has gone from being a side project used for creative passion projects, to a full-time business with clients from varying market sectors including government, property, sport and entertainment.</p>
+
+          <a href="#" class="button button--left button--dark">learn more <strong>about us</strong></a>
+
+        </div>
+
+        <img alt="Chop Shop Media Photography" style="margin-top: 80px" src="<?php image_directory() ?>/content-home-section-01.jpg">
+
+      </div>
 
     </div>
 
