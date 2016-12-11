ee64092f4a03fc5c44d19bfe39813001836e5d2b
Ryan <hey@mix-tape.io>
2016-12-11 13:09:52 +0800

    Add Instagram link in footer

--- wp-content/themes/chopshopmedia/templates/layout/footer.php
+++ wp-content/themes/chopshopmedia/templates/layout/footer.php
@@ -7,7 +7,7 @@
 
       <nav class="shareLinks pageFooter-shareLinks">
         <a target="_blank" href="http://www.facebook.com/chopshopmedia" class="shareLinks-link icon icon-facebook"></a>
-        <a target="_blank" href="#" class="shareLinks-link icon icon-instagram"></a>
+        <a target="_blank" href="https://www.instagram.com/chop_shop_media" class="shareLinks-link icon icon-instagram"></a>
         <a target="_blank" href="http://www.linkedin.com/profile/view?id=108064063&trk=tab_pro" class="shareLinks-link icon icon-linkedin"></a>
         <a target="_blank" href="#" class="shareLinks-link icon icon-youtube"></a>
       </nav>
