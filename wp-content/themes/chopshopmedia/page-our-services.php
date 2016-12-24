1aa4ae85984e6c92586aecc011fa9dcf2ea0f62e
Ryan <hey@mix-tape.io>
2016-12-24 20:17:31 +0800

    Start our services

---
+++ wp-content/themes/chopshopmedia/page-our-services.php
@@ -0,0 +1,3 @@
+<?php get_template_part('templates/layout/page-title'); ?>
+<?php get_template_part('templates/content/our-services'); ?>
+<?php get_template_part( 'templates/layout/call-to-action' ); ?>
---
+++ wp-content/themes/chopshopmedia/templates/content/our-services.php
@@ -0,0 +1,27 @@
+
+  <section class="wrapper pageSection">
+
+    <div class="container">
+
+      <?php while (have_posts()) : the_post(); ?>
+
+        <nav class="sidebarNavigation">
+
+          <a class="sidebarNavigation-menuItem" href="/about-us">01</a>
+          <!-- <a class="sidebarNavigation-menuItem" href="/our-services">02</a> -->
+          <a class="sidebarNavigation-menuItem" href="/our-work">03</a>
+          <a class="sidebarNavigation-menuItem" href="/contact-us">04</a>
+
+        </nav>
+
+        <article <?php post_class('pageContent') ?> id="post-<?php the_ID(); ?>">
+
+          <?php the_content( ); ?>
+
+        </article>
+
+      <?php endwhile; ?>
+
+    </div>
+
+  </section>
