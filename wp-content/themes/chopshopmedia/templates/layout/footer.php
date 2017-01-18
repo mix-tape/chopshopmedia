5f9e54444815b2d207c5b705cd19c7633d8be875
Ryan <hey@mix-tape.io>
2017-01-18 12:28:11 +0100

    Add Vimeo logo icon and footer link, tweak padding on quote slider

--- wp-content/themes/chopshopmedia/templates/layout/footer.php
+++ wp-content/themes/chopshopmedia/templates/layout/footer.php
@@ -9,7 +9,7 @@
         <a target="_blank" href="http://www.facebook.com/chopshopmedia" class="shareLinks-link icon icon-facebook"></a>
         <a target="_blank" href="https://www.instagram.com/chop_shop_media" class="shareLinks-link icon icon-instagram"></a>
         <a target="_blank" href="http://www.linkedin.com/profile/view?id=108064063&trk=tab_pro" class="shareLinks-link icon icon-linkedin"></a>
-        <a target="_blank" href="#" class="shareLinks-link icon icon-youtube"></a>
+        <a target="_blank" href="https://vimeo.com/user5910871" class="shareLinks-link icon icon-vimeo"></a>
       </nav>
 
     </div>
